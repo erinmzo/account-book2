1d8acead9ee9fad8b274480bf56eeeec0ab51363
Minsoo Cho <textign@gmail.com>
2024-06-11 14:31:03 +0900

    account-base-url 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const AUTH_BASE_URL = "https://moneyfulpublicpolicy.co.kr";
 
-export const ACCOUNT_BASE_URL = "http://localhost:3000";
+export const ACCOUNT_BASE_URL = "https://rune-rightful-wholesaler.glitch.me";
 
 export const MONTH = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
